11cf520535c2a0c60567182ab968aa4cfbdba2f8
eridyms <61139169+eridyms@users.noreply.github.com>
2025-07-01 21:51:37 +0100

    Create sidebar.php

---
+++ includes/sidebar.php
@@ -0,0 +1,8 @@
+<div class="sidebar">
+  <h2>Refund Dashboard</h2>
+  <ul>
+    <li><a href="#">Dashboard</a></li>
+    <li><a href="#">Reports</a></li>
+    <li><a href="#">Settings</a></li>
+  </ul>
+</div>
